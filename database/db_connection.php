26966f822e951c92462bbf7c6d48d70dec5906b1
BennyJetz <benlange439@gmail.com>
2025-10-21 07:31:53 -0500

    Created file to connect MAMP database to the program.

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,13 +1,15 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root"; // default for MAMP
+$password = "root";
 $dbname = "byte2bite_db";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
-echo "Database connected successfully!";
+echo "Connected successfully";
 ?>
